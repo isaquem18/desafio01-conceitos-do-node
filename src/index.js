ee1407f96e4ee9fd86098781a2d5d1bf7d048c05
Isaque Maninde Moura Patricio <isaquem18@gmail.com>
2022-05-06 00:04:47 -0300

    all

--- src/index.js
+++ src/index.js
@@ -1,41 +1,176 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+  try {
+
+    const { username } = req.headers;
+
+    const findUser = users.find(item => String(item.username) === String(username));
+
+    if (!findUser) {
+      return res.status(404).json({
+        error: 'User not found.'
+      });
+    };
+
+    req.user = findUser;
+    
+    next();
+
+  } catch (e) {
+
+    return res.status(500).send();
+
+  }
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', (req, res) => {
+  try {
+
+    const { name, username } = req.body;
+
+    const checkUserExistency = users.find(item => item.username === username);
+
+    if (checkUserExistency) return res.status(400).json({
+      error: 'Users already has been created.'
+    });
+
+    const newUser = {
+      id: v4(),
+      name,
+      username,
+      todos: []
+    };
+
+    users.push(newUser);
+
+    return res.status(201).json(newUser);
+
+
+  } catch(e) {
+
+    return res.status(500).send();
+
+  }  
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get('/todos', checksExistsUserAccount, (req, res) => {
+  try {
+
+    const { user } = req;
+
+    return res.status(200).json(user.todos);
+
+  } catch(e) {
+
+    return res.status(500).send();
+
+  }
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post('/todos', checksExistsUserAccount, (req, res) => {
+  try {
+
+    const { user } = req;
+
+    const { title, deadline } = req.body;
+
+    const newTodo = {
+      id: v4(),
+      title,
+      done: false,
+      deadline: new Date(deadline),
+      created_at: new Date()
+    };
+    
+    user.todos.push(newTodo);
+
+    return res.status(201).json(newTodo);
+
+  } catch (e) {
+    return res.status(500).send();
+  }
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
+  try {
+
+    const { user } = req;
+    const { id } = req.params;
+    const { title, deadline } = req.body;
+
+    const specificTodo = user.todos.find(item => item.id === id);
+
+    if (!specificTodo) {
+      return res.status(404).json({
+        error: 'Todo item not found.'
+      });
+    }
+
+    specificTodo.title = title;
+    specificTodo.deadline = new Date(deadline);
+
+    return res.status(200).json(specificTodo);
+
+  } catch (e) {
+    return res.status(500).send();
+  }
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
+  try {
+
+    const { user } = req;
+    const { id } = req.params;
+
+    const todo = user.todos.filter(item => item.id === id)[0];
+
+    if (!todo) {
+      return res.status(404).json({ 
+        error: 'Todo item not found.'
+      })
+    }
+
+    todo.done = !todo.done
+
+    return res.status(200).json(todo);
+
+  } catch(e) {
+    return res.status(500).send();
+  }
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
+  try {
+
+    const { user } = req;
+    const { id } = req.params;
+
+    const todoExistency = user.todos.some(item => item.id === id);
+
+    if (!todoExistency) {
+      return res.status(404).json({
+        error: 'error'
+      });
+    }
+
+    const newTodoList = user.todos.filter(item => item.id !== id);
+    user.todos = newTodoList;
+
+    return res.status(204).send();
+
+  } catch(e) {
+    return res.status(500).send();
+  }
 });
 
 module.exports = app;
